29ebd2a9a09034c091e0633f1033149d16a10d7e
elwinpinto11 <85425959+elwinpinto11@users.noreply.github.com>
2021-06-06 10:11:12 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>
+        This camp was conducted by Shaurya Sinha from ShapeAI and it was an
+        amazing camp. It covered everythimg of Javascript, React.js and HTML
+        from scratch and it benefitted a lot from this bootcamp.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
